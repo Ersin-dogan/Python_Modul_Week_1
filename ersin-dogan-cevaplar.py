c8c5acc8499620dc45b042b7a0b58e861fd03f81
Ersin-dogan <ersindogan@hotmail.com>
2024-09-02 22:25:40 +0200

    Create ersin-dogan-cevaplar.py

---
+++ ersin-dogan-cevaplar.py
@@ -0,0 +1,46 @@
+######### ilk sorunun cevabı
+for sayi in range(1,11):
+    print(sayi)
+
+
+#########// soru-2 cevabı
+#for döngüsü
+for sayi in range(0, 10 + 1, 2):
+    print(sayi)
+
+# while döngüsü
+sayial = int(input("Bir sayı giriver : "))
+basla = 2
+while basla <= sayial:
+    print(basla)
+    basla += 2 
+
+
+#########// soru-3 cevabı
+ilksayi = input("Lütfen ilk sayıyı giriniz !")
+ikincisayi = input("Lütfen ikinci sayıyı giriniz !")
+
+for sayi in range(int(ilksayi), int(ikincisayi)+1):
+    print(sayi)
+
+
+
+#########// soru-4 cevabı
+sayial = input("Lütfen bir sayı giriver: ")
+if sayial % 2 == 0:
+    print(f"{sayial} Girdiğin sayı çift sayıdır.")
+else:
+    print(f"{sayial} Girdiğin sayı tek sayıdır.")
+
+
+#########// soru-5 cevabı
+
+
+
+
+
+
+
+
+
+
